08ec3fc8304fc7daebbbaf81a7c56d4532083dc4
David Rosenblum <dave@savet.net>
2021-07-08 17:05:48 -0400

    Add deployment name to IP scripts
    Add deployment name to IP scripts in extras
    This allows you to have ip addresses per deployment

--- tools/example-scripts/extras/createIPAddress-GKE.sh
+++ tools/example-scripts/extras/createIPAddress-GKE.sh
@@ -1,10 +1,11 @@
+#This example reflects the information in https://neo4j.com/labs/neo4j-helm/1.0.0/externalexposure/#_create_static_ip_addresses_for_inbound_cluster_traffic
 for idx in 0 1 2 ; do
    gcloud compute addresses create \
-      neo4j-static-ip-$idx --project=$PROJECT \
+      ${DEPLOYMENT}.neo4j-static-ip-$idx --project=$PROJECT \
       --network-tier=PREMIUM --region=$REGION
 
-    export IP$idx=`gcloud compute addresses describe neo4j-static-ip-$idx \
+    export IP$idx=`gcloud compute addresses describe ${DEPLOYMENT}.neo4j-static-ip-$idx \
       --region=$REGION --project=$PROJECT --format=json | jq -r '.address'`
-    export ADDR$idx=`gcloud compute addresses describe neo4j-static-ip-$idx \
+    export ADDR$idx=`gcloud compute addresses describe ${DEPLOYMENT}.neo4j-static-ip-$idx \
       --region=$REGION --project=$PROJECT --format=json | jq -r '.address'`
 done
